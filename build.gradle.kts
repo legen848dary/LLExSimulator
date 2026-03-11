c202bcea60a647f584aad14d9a8351bb58ac2ed1
Debjyoti Sarkar <dj.iitian@gmail.com>
2026-03-12 03:23:50 +0800

    fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "com.llexsimulator"
